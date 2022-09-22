1e4eeb107bc323f2687c28c98aef94d894dfaa09
mdlawrenceusa <mdlawrence@me.com>
2022-09-22 11:52:14 -0400

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   imports: [
@@ -13,12 +14,16 @@ import { ProductListComponent } from './product-list/product-list.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'book', component: BookComponent },
+
+
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    BookComponent
   ],
   bootstrap: [
     AppComponent
